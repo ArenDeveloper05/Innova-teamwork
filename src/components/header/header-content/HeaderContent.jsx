84b5385e56f50f907656a89e03bef676f976414d
ArenDeveloper05 <antisystem005@mail.ru>
2022-08-02 17:01:35 +0400

    INSERTING HEADER SIDE (AREN)

---
+++ src/components/header/header-content/HeaderContent.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import '../../header/header-content/HeaderContent.scss'
+const HeaderContent = () => {
+  return (
+   
+
+      <div className="header-content">
+        <div className="header-content-color"></div>
+        <div className='header-content-desc'>
+          <h1>Outsource Your Development</h1>
+          <p>Outsource Your Development at Affordable Prices to Senior Engineers in Armenia</p>
+          <div className="header-content-desc-buttons">
+            <button>Free Estimation</button>
+          </div>
+        </div>
+
+    </div>
+
+  
+
+    
+  )
+}
+
+export default HeaderContent
